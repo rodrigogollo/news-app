67aa595075f196ddb59584b7a1bd239f249b32b9
Rodrigo Gollo <rodrigogollo.nh@gmail.com>
2025-01-02 23:09:43 -0300

    remove old files from root, and changed some design

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
-	return ( 
-		<>
-			<h1>Vozes do Gigante</h1>
-		</>
-	)
+  return (
+    <>
+      <h1 className="p-4 text-3xl font-bold text-center text-red-800 uppercase">Vozes do Gigante</h1>
+    </>
+  )
 }
 
 export default Header;
--- app/src/components/Navbar/Navbar.tsx
+++ app/src/components/Navbar/Navbar.tsx
@@ -1,18 +1,16 @@
-import "./Navbar.css"
-
 const Navbar = () => {
-	return (
-		<nav id="home">
-			<ul>
-				<a href="#home"><li>Home</li></a>
-				<a href="#footer"><li>Últimas Notícias</li></a>
-				<a href="#"><li>Mercado da Bola</li></a>
-				<a href="#"><li>Nosso Time</li></a>
-				<a href="#"><li>Parceiros</li></a>
-				<a href="#"><li>Programas</li></a>
-			</ul>
-		</nav>
-	)
+  return (
+    <nav className="">
+      <ul className="flex flex-row justify-evenly p-4 bg-red-600 text-white ">
+        <a className="hover:underline" href="#home"><li>Home</li></a>
+        <a className="hover:underline" href="#footer"><li>Últimas Notícias</li></a>
+        <a className="hover:underline" href="#"><li>Mercado da Bola</li></a>
+        <a className="hover:underline" href="#"><li>Nosso Time</li></a>
+        <a className="hover:underline" href="#"><li>Parceiros</li></a>
+        <a className="hover:underline" href="#"><li>Programas</li></a>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar;
