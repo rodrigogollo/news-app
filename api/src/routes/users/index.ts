28cc156e2312dc1dc08489e583626226664fa08d
Rodrigo Gollo <rodrigogollo.nh@gmail.com>
2025-01-08 21:39:26 -0300

    add example user routes

--- api/src/routes/users/index.ts
+++ api/src/routes/users/index.ts
@@ -2,13 +2,27 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("users route testing");
+app.get("/", (_, res) => {
+  res.send("Get Users");
+})
+
+app.get("/:id", (req, res) => {
+  let id = req.query.id;
+  res.send(`Get User with id of ${id}`);
 })
 
 app.post("/user", (req, res) => {
-  res.send("users route testing");
+  let body = req.body;
+  res.send(`Create new user ${body}`);
 })
 
+app.put("/user/:id", (req, res) => {
+  let id = req.query.id;
+  res.send(`Edit User with id of ${id}`);
+})
 
+app.delete("/user/:id", (req, res) => {
+  let id = req.query.id;
+  res.send(`Delete User with id of ${id}`);
+})
 export default app;
