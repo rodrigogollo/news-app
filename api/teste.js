dfe09013353a5996b16218a7288b6d67b8a64da6
Rodrigo Gollo <rodrigogollo.nh@gmail.com>
2025-01-14 08:18:40 -0300

    reading example headers Markdown file

--- api/teste.js
+++ api/teste.js
@@ -1,7 +1,26 @@
-const { open } = require('node:fs/promises');
+import { open } from 'node:fs/promises';
 
 const file = await open("./example.md");
 
+let header = {}
+let isHeader = false;
 for await (const line of file.readLines()) {
-    console.log(line);
+  //console.log(line);
+
+  if (line == "```") {
+    isHeader = !isHeader
+    continue;
+  }
+
+  if (isHeader) {
+    let key = line.split(": ")[0];
+    let value = line.split(": ")[1];
+    if (value.includes("[")) {
+      header[key] = value.slice(1, -1).split(',').map(item => item.trim());
+    } else {
+      header[key] = value;
+    }
+  }
 }
+
+console.log("header", header);
