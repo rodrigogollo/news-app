49b0f1b19e36bf64f314d44c9d1782a8609bcc39
Rodrigo Gollo <rodrigogollo.nh@gmail.com>
2025-02-09 18:41:14 -0300

    add Loading icon and spin animation

--- app/src/components/Loading/Loading.tsx
+++ app/src/components/Loading/Loading.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import "./Loading.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
 const Loading = () => {
   return (
-    <span>Loading...</span>
+    <span className="text-red-600 animate-spin text-2xl w-screen text-center">
+      <FontAwesomeIcon className="animate-spin" icon={faCircleNotch} size="2xl" />
+    </span>
   )
 }
 
